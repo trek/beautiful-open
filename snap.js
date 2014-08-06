09233e205b6621bac27c55609f656521ae69225f
Trek Glowacki <trek@groupon.com>
2014-08-06 09:28:17 -0500

    update capture script

--- snap.js
+++ snap.js
@@ -5,37 +5,29 @@
 var args = require('system').args;
 var site = args[1];
 var name = args[2];
+var timeout = args[3] || 0;
+var sizes = [1000, 800];
 
-var async = require('async'),
-    sizes = [
-        [1000, 800]
-    ];
-
-function capture(sizes, callback) {
-    var page = require('webpage').create();
-    page.viewportSize = {
-        width: sizes[0],
-        height: sizes[1]
-    };
-    page.clipRect = {
-      top: 0,
-      left: 0,
-      width: sizes[0],
-      height: sizes[1]
-    };
-    page.zoomFactor = 0.9;
-    page.open(site, function (status) {
-        setTimeout(function(){
-            var filename = name + '.png';
-            page.render('./screenshots/' + filename);
-            page.close();
-            callback.apply();
-        }, 2000);
-    });
-}
-
-async.eachSeries(sizes, capture, function (e) {
-    if (e) console.log(e);
+var page = require('webpage').create();
+page.viewportSize = {
+    width: sizes[0],
+    height: sizes[1]
+};
+page.clipRect = {
+  top: 0,
+  left: 0,
+  width: sizes[0],
+  height: sizes[1]
+};
+page.zoomFactor = 0.9;
+page.open(site, function (status) {
+  setTimeout(function(){
+    var filename = name + '.png';
+    page.render('./screenshots/' + filename);
+    page.close();
     console.log('done!');
     phantom.exit();
+  }, timeout);
 });
+
+
